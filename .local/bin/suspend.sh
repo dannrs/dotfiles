265bf75ad523bc143c0701bcbdd4e15205efb3bf
n3ira <n3iraprojects@gmail.com>
2022-03-30 23:52:33 +0700

    Change some settings

--- .local/bin/suspend.sh
+++ .local/bin/suspend.sh
@@ -1,1 +1,1 @@
-xidlehook --detect-sleep --not-when-fullscreen --not-when-audio --timer 60 'xset dpms force off' '' --timer 90 '$HOME/.local/bin/lock' '' --timer 300 'systemctl suspend' '' &
+xidlehook --detect-sleep --not-when-fullscreen --not-when-audio --timer 300 'xset dpms force off' '' --timer 360 '$HOME/.local/bin/lock' '' --timer 1500 'systemctl suspend' '' &
